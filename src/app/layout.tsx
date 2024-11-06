83beb849a28ab54617ed788e9382b81be800b8c9
William Gyasi <gyasi.wk@gmail.com>
2024-11-06 22:51:09 +0000

    Preparing For Rollout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={``}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable}`}>{children}</body>
     </html>
   );
 }
