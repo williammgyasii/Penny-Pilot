c4d7f4baaa47d5be52cee3dfbd2ec5e404431c2b
William Gyasi <gyasi.wk@gmail.com>
2024-11-07 00:18:35 +0000

    Launch On Dev

--- src/app/(landingPage)/page.tsx
+++ src/app/(landingPage)/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-
 const LandingPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center text-6xl">
+      Added Homepage For Launch
+    </div>
+  );
 };
 
 export default LandingPage;
