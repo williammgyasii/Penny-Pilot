47d71e6c2b84f80a69a7f3f6f8777af6be2dbe00
William Gyasi <gyasi.wk@gmail.com>
2024-11-07 00:31:00 +0000

    Added Some Test Elements

--- src/app/(landingPage)/page.tsx
+++ src/app/(landingPage)/page.tsx
@@ -1,9 +1,14 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center text-6xl">
-      Added Homepage For Launch
+    <div className="min-h-screen flex items-center flex-col justify-center text-6xl space-y-10">
+      <h1>Added Homepage For Launch</h1>
+      <Link href={"/about"} className={buttonVariants({ variant: "default" })}>
+        Click here
+      </Link>
     </div>
   );
 };
