f333309b801b1ac0add0a29b753065e29cc3e182
William Gyasi <gyasi.wk@gmail.com>
2024-11-08 00:39:24 +0000

    Hero Respons

--- src/app/(landing)/_sections/HeroSection.tsx
+++ src/app/(landing)/_sections/HeroSection.tsx
@@ -41,18 +41,20 @@ const HeroSection = () => {
           </TimelineContent>
         </div>
 
-        <TimelineContent animationNum={3}>
-          <div
-            className="lg:w-[48rem] p-1 [background:linear-gradient(45deg,#080b11,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] 
+        <div className="col-span-1">
+          <TimelineContent animationNum={3}>
+            <div
+              className="lg:w-[48rem] p-1 [background:linear-gradient(45deg,#080b11,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] 
         rounded-2xl border-2 border-transparent animate-border"
-          >
-            <Image
-              src={heroSection}
-              className="rounded-2xl"
-              alt="Hero Illustration"
-            />
-          </div>
-        </TimelineContent>
+            >
+              <Image
+                src={heroSection}
+                className="rounded-2xl"
+                alt="Hero Illustration"
+              />
+            </div>
+          </TimelineContent>
+        </div>
       </div>
     </SectionLayout>
   );
