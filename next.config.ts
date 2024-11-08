c732251e9c510781f86612ae3c264d961a10989f
William Gyasi <gyasi.wk@gmail.com>
2024-11-08 06:48:27 +0000

    Tubr

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["images.unsplash.com"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
