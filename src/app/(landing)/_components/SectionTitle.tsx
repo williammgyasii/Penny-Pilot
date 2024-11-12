9c385d0b85a0f30ca9399d2157c0a9716b34dc37
William Gyasi <gyasi.wk@gmail.com>
2024-11-12 17:39:16 +0000

    Cnar

--- src/app/(landing)/_components/SectionTitle.tsx
+++ src/app/(landing)/_components/SectionTitle.tsx
@@ -1,17 +1,24 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 type Props = {
   title: string;
   subtitle?: string;
+  dark?: boolean;
 };
 
-const SectionTitle = ({ title, subtitle }: Props) => {
+const SectionTitle = ({ title, subtitle, dark }: Props) => {
   return (
     <>
       <h2
-        className="text-4xl text-center md:text-4xl lg:text-5xl xl:text-7xl
+        className={cn(
+          `text-4xl text-center md:text-4xl lg:text-5xl xl:text-7xl
             text-balance w-full md:w-full lg:w-[80%]  md:leading-relaxed tracking-tighter 
-            font-poppins font-medium text-ui-ui_light_200"
+            font-poppins font-medium text-ui-ui_light_200`,
+          {
+            "text-black": dark,
+          }
+        )}
       >
         {title}
       </h2>
--- src/app/(landing)/_sections/ExceptionalSection.tsx
+++ src/app/(landing)/_sections/ExceptionalSection.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import SectionLayout from "../_components/SectionLayout";
+import { ScrollFadeInEffect } from "@/animated/ScrollFadeInEffect";
+import TitleBubble from "../_components/TitleBubble";
+import SectionTitle from "../_components/SectionTitle";
 
 const ExceptionalSection = () => {
   return (
@@ -8,15 +11,24 @@ const ExceptionalSection = () => {
       className="bg-ui-ui_light_200 -mt-2 py-[4rem] px-0 md:!px-0 "
     >
       <div
-        className="absolute top-1/4 left-0 -translate-x-2/3 -translate-y-1/2
+        className="absolute top-1/4 left-0 -translate-x-2/3
         w-[500px] h-[500px] rounded-full
         bg-blue-500/30 
         blur-3xl 
         animate-[pulse_8s_ease-in-out_infinite]
         hover:bg-blue-500/40 transition-colors duration-2000"
       />
-      <div>
-
+      <div className="flex flex-col items-center justify-center w-full">
+        <ScrollFadeInEffect fullWidth="100%" animationNum={0}>
+          <div className="md:space-y-5 space-y-3 items-center flex flex-col">
+            <TitleBubble title="Financial Disipline Made Easy" variant="blue" />
+            <SectionTitle
+              dark
+              subtitle="Explore what makes us exceptional in the finance industry. Experience innovative solutions, personalized services, and seamless transactions."
+              title="What makes us exceptional"
+            />
+          </div>
+        </ScrollFadeInEffect>
       </div>
     </SectionLayout>
   );
