04acb42e0472f2e36fcdb312fc853f8eaa009251
William Gyasi <gyasi.wk@gmail.com>
2024-12-01 10:12:46 +0000

    Login Schema

---
+++ src/app/(auth)/_components/RegisterForm.tsx
@@ -0,0 +1,168 @@
+"use client";
+import GoogleSignInButton from "@/components/GoogleSignInButton";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useAppDispatch } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
+import { REGISTER_SCHEMA, TYPE_REGISTER_SCHEMA } from "@/schema/registerSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+import PasswordRequirements from "./PasswordRequirements";
+import PasswordStrengthIndicator from "./PasswordStrengthIndicator";
+import { useRouter } from "next/navigation";
+import { addToast } from "@/redux/features/toastSlice";
+import { registerUser } from "@/redux/asyncfunctions/authFunctions";
+
+export default function RegisterForm() {
+  const form = useForm<TYPE_REGISTER_SCHEMA>({
+    resolver: zodResolver(REGISTER_SCHEMA),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const { AUTH_SLICE_LOADING } = useSelector((state: RootState) => state.auth);
+
+  const onSubmit = form.handleSubmit(async (data: TYPE_REGISTER_SCHEMA) => {
+    try {
+      const result = await dispatch(registerUser(data)).unwrap();
+      if (result) {
+        router.push("/dashboard/overview");
+        form.reset();
+        dispatch(
+          addToast({
+            message: "Account created successfully!",
+            type: "success",
+          })
+        );
+      }
+      form.reset();
+    } catch (error: unknown) {
+      dispatch(
+        addToast({
+          message: (error as string) || "Registration failed",
+          type: "error",
+        })
+      );
+    }
+  });
+
+  return (
+    <>
+      <Form {...form}>
+        <div className=" w-[90%]  xl:w-[85%]">
+          <form onSubmit={onSubmit} className="space-y-2">
+            <div className="grid grid-cols-2 w-full gap-2">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="col-span-2 lg:col-span-1">
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        className="focus:border-cyan-700"
+                        placeholder="First Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className=" col-span-2 lg:col-span-1">
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        className="focus:border-cyan-700"
+                        placeholder="Last Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      className="focus:border-cyan-700"
+                      placeholder="Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      minLength={10}
+                      type="password"
+                      className="focus:border-cyan-700"
+                      placeholder="************"
+                      {...field}
+                    />
+                  </FormControl>
+                  {form.getValues("password") && (
+                    <PasswordStrengthIndicator
+                      passwordValue={form.getValues("password")}
+                    />
+                  )}
+                  {form.getValues("password") && (
+                    <PasswordRequirements password={field.value} />
+                  )}
+
+                  {/* <FormMessage /> */}
+                </FormItem>
+              )}
+            />
+
+            <Button
+              className="w-full "
+              isLoading={AUTH_SLICE_LOADING}
+              disabled={form.formState.isSubmitting}
+              type="submit"
+            >
+              Register
+            </Button>
+          </form>
+          <GoogleSignInButton />
+        </div>
+      </Form>
+    </>
+  );
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,59 @@
-"use client";
-import { RootState } from "@/redux/store";
+import { Metadata } from "next";
 import React from "react";
-import { useSelector } from "react-redux";
+import registerBackground from "@public/Fintap.png";
+import Image from "next/image";
+import TextGradient from "@/animated/TextGradient";
+
+export const metadata: Metadata = {
+  title: "Register Page",
+  description: "Getting started to financial freedom",
+  keywords: ["penny pilot", "register"],
+};
 
 const LoginPage = () => {
-  const { currentUser } = useSelector((state: RootState) => state.auth);
-  console.log(currentUser);
-  return <div>Login Page</div>;
+  return (
+    <div className="min-h-screen bg-ui-ui_light_200 grid grid-cols-8 p-2 lg:space-x-1 md:space-x-4">
+      <div className="col-span-8 md:col-span-4 h-[20rem] md:h-full bg-yellow-900 relative rounded-xl overflow-hidden">
+        <Image
+          fill
+          src={registerBackground}
+          className="w-full h-full object-cover z-10"
+          alt="Register Background"
+          sizes={"auto"}
+          priority={true}
+        />
+        {/* Gradient */}
+        <div className="absolute z-10 inset-0 bg-gradient-to-t from-black  to-black/30 opacity-95"></div>
+
+        {/* Text Content */}
+        <div className="absolute z-30  bottom-3 md:bottom-12 xl:bottom-40  md:left-5 w-full md:w-[80%]">
+          <h1 className="text-center text-4xl md:text-left md:text-7xl lg:text-8xl font-poppins  font-medium md:leading-[3.7rem] lg:leading-[5rem] tracking-normal lg:tracking-tighter text-transparent bg-clip-text bg-gradient-to-b from-cyan-200 via-cyan-400 to-cyan-800">
+            Chart a course to financial freedom
+            <span className="inline-block text-base md:text-2xl tracking-normal text-white">
+              —starting with a penny!
+            </span>
+          </h1>
+
+          <blockquote className="text-center inline-block mt-5 w-[95%] italic text-xs md:text-sm font-medium font-poppins text-white">
+            {`"The habit of saving is itself an education; it fosters every
+            virtue, teaches self-denial, cultivates the sense of order, trains
+            to forethought, and so broadens the mind" - T.T. Munger`}
+          </blockquote>
+        </div>
+      </div>
+
+      <div className="col-span-8 md:col-span-4 h-full lg:-ml-2 py-5 md:py-[4rem]">
+        <div className="flex flex-col items-center justify-center w-full lg:px-[3rem] xl:px-[5rem]">
+          <TextGradient
+            text="Create Account"
+            subtitle="Getting started on your path to freedom"
+          />
+          <RegisterForm />
+          {/* <RegisterForm /> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
