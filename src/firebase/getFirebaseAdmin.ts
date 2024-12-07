9a941c2ad3b0c2a7b3a4a04dadd3919cab39cdd0
William Gyasi <gyasi.wk@gmail.com>
2024-12-07 02:44:11 +0000

    Create Session Cookie

--- src/firebase/getFirebaseAdmin.ts
+++ src/firebase/getFirebaseAdmin.ts
@@ -1,12 +1,13 @@
-// src/lib/firebaseAdmin.js
-import { initializeApp, applicationDefault } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
+import admin from "firebase-admin";
 
-// Initialize Firebase Admin SDK
-initializeApp({
-  credential: applicationDefault(), // Use service account or default credentials
-});
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
-const firebaseAdminAuth = getAuth(); // Firebase authentication instance
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
 
-export { firebaseAdminAuth };
+export const adminAuth = admin.auth();
+export const adminFirestore = admin.firestore();
