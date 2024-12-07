2724c48759c93aed6314fd8e26c5a8473e8de9ed
William Gyasi <gyasi.wk@gmail.com>
2024-12-07 22:25:31 +0000

    Logout

--- src/redux/functions/authFunctions.ts
+++ src/redux/functions/authFunctions.ts
@@ -81,10 +81,11 @@ export const LOGIN_EXISTING_USER = createAsyncThunk<
 
     const idToken = await user.getIdToken();
     if (idToken) {
-      const results = await axios.post(
-        CREATE_SESSION_COOKIE_CLOUD_FUNCTION_URL,
-        { idToken }
-      );
+      const results = await axios.post("/api/auth/session", { idToken });
+      // const results = await axios.post(
+      //   CREATE_SESSION_COOKIE_CLOUD_FUNCTION_URL,
+      //   { idToken }
+      // );
       console.log(results);
     }
     return { ...user, ...userDetails };
