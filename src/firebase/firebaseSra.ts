a69cc80deea894d786021f11e065ca42da6c72ba
William Gyasi <gyasi.wk@gmail.com>
2024-12-07 23:54:00 +0000

    Surrenc

--- src/firebase/firebaseSra.ts
+++ src/firebase/firebaseSra.ts
@@ -1,19 +1,17 @@
 import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
-import * as admin from "firebase-admin";
+import admin, { initializeApp } from "firebase-admin";
 
 // Google Cloud Secret Manager client
 const client = new SecretManagerServiceClient();
-let initialized = false;
 
 // Securely fetch and initialize Firebase Admin credentials
 async function initializeFirebaseAdmin() {
-  if (initialized) return;
-
   const secretName =
     "projects/akron-server/secrets/firebase-admin-credentials/versions/latest";
 
   try {
     const [version] = await client.accessSecretVersion({ name: secretName });
+    console.log(version);
 
     if (!version?.payload?.data) {
       throw new Error("Secret payload is null or undefined");
@@ -21,18 +19,15 @@ async function initializeFirebaseAdmin() {
 
     const serviceAccount = JSON.parse(version.payload.data.toString());
 
-    admin.initializeApp({
+    initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
-
-    initialized = true;
     console.log("Firebase Admin Triggered successfully...");
   } catch (error) {
     console.error(
       "Failed to initialize Firebase Admin with Secret Manager:",
       error
     );
-    throw new Error("Failed to initialize Firebase Admin");
   }
 }
 
