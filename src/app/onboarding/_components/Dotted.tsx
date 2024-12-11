db9c894e941a051819514d26edbcaf1ebf4c32e9
William Gyasi <gyasi.wk@gmail.com>
2024-12-11 11:16:00 +0000

    Ui Dark

---
+++ src/app/onboarding/_components/Dotted.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Dotted = () => {
+  return (
+    <div>Dotted</div>
+  )
+}
+
+export default Dotted
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useAppDispatch, useAppSelector } from "@/redux/reduxhooks";
+import { useAppSelector } from "@/redux/reduxhooks";
 import { RootState } from "@/redux/store";
 import React from "react";
 
@@ -8,8 +8,21 @@ const OnboardingPage = () => {
   console.log(currentUser);
 
   return (
-    <div className="">
-      Welcome to onboaacrding
+    <div className="py-10 space-y-2 flex flex-col items-center justify-center ">
+      {/* <TextGradient
+        text="Welcome Back"
+        subtitle="Login to continue with your financial discipline"
+      /> */}
+      <div>
+        <h1 className="text-3xl text-center">Basic Details</h1>
+        <span className="text-ui-ui_light_600  text-sm">
+          Please fill the fields below with your contact details.
+        </span>
+      </div>
+
+      <div className="bg-ui-ui_dark_200 w-full">
+        
+      </div>
     </div>
   );
 };
