28b9aaeb29dbd9b55e15fd5fdf7b624d0d8dddc3
William Gyasi <gyasi.wk@gmail.com>
2024-12-13 08:16:18 +0000

    Ac

--- src/app/onboarding/_components/FormProgressIndicator.tsx
+++ src/app/onboarding/_components/FormProgressIndicator.tsx
@@ -10,16 +10,16 @@ export default function FormProgressIndicator({
   totalSteps,
 }: ProgressIndicatorProps) {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center">
       {Array.from({ length: totalSteps }, (_, i) => (
-        <div key={i} className="flex items-center">
+        <div key={i} className="flex items-center ">
           <div
-            className={`rounded-full h-3 w-3 flex items-center justify-center ${
+            className={`rounded-full h-7 w-7 flex items-center justify-center ${
               i <= currentStep ? "bg-primary" : "bg-muted"
             }`}
           >
             {i < currentStep && (
-              <Check className="h-2 w-2 text-primary-foreground" />
+              <Check className="h-5 w-5 text-primary-foreground" />
             )}
           </div>
           {i < totalSteps - 1 && (
