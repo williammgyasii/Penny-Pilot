1e49102261efe64bcb90777884b5d8fc7158fcea
William Gyasi <gyasi.wk@gmail.com>
2025-01-05 22:43:18 +0000

    Building In Monochrome

--- src/app/dashboard/onboarding/steps/FinancialGoals.tsx
+++ src/app/dashboard/onboarding/steps/FinancialGoals.tsx
@@ -21,65 +21,9 @@ export default function FinancialGoals() {
   const { control } = useFormContext<TYPE_ONBOARDING_SCHEMA>();
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-2">
       <h2 className="text-2xl font-bold">Financial Goals</h2>
-      <FormField
-        control={control}
-        name="primaryGoal"
-        render={({ field }) => (
-          <FormItem className="space-y-3">
-            <FormLabel>Notify me about...</FormLabel>
-            <FormControl>
-              <RadioGroup
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                className="flex flex-col space-y-1"
-              >
-                <FormItem className="flex items-center space-x-3 space-y-0">
-                  <FormControl>
-                    <RadioGroupItem value="all" />
-                  </FormControl>
-                  <FormLabel className="font-normal">
-                    All new messages
-                  </FormLabel>
-                </FormItem>
-                <FormItem className="flex items-center space-x-3 space-y-0">
-                  <FormControl>
-                    <RadioGroupItem value="mentions" />
-                  </FormControl>
-                  <FormLabel className="font-normal">
-                    Direct messages and mentions
-                  </FormLabel>
-                </FormItem>
-                <FormItem className="flex items-center space-x-3 space-y-0">
-                  <FormControl>
-                    <RadioGroupItem value="none" />
-                  </FormControl>
-                  <FormLabel className="font-normal">Nothing</FormLabel>
-                </FormItem>
-              </RadioGroup>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-        // <FormItem>
-        //   <FormLabel>Primary Financial Goal</FormLabel>
-        //   <Select onValueChange={field.onChange} defaultValue={field.value}>
-        //     <FormControl>
-        //       <SelectTrigger>
-        //         <SelectValue placeholder="Select a goal" />
-        //       </SelectTrigger>
-        //     </FormControl>
-        //     <SelectContent>
-        //       <SelectItem value="savings">Savings</SelectItem>
-        //       <SelectItem value="investment">Investment</SelectItem>
-        //       <SelectItem value="debt_repayment">Debt Repayment</SelectItem>
-        //       <SelectItem value="retirement">Retirement</SelectItem>
-        //     </SelectContent>
-        //   </Select>
-        //   <FormMessage />
-        // </FormItem>
-      />
+      
       <FormField
         control={control}
         name="targetAmount"
